1ea717f201ceb73545cf5f24a5fafde76acd3be5
Keerthivasan S <keerthi.s@accolite.com>
2022-12-05 18:19:54 +0530

    Problem-2 done

--- src/app/modules/vendor/vendor.module.ts
+++ src/app/modules/vendor/vendor.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { VendorRoutingModule } from './vendor-routing.module';
 import { ActiveVendorComponent } from './active-vendor/active-vendor.component';
-
+import { ReactiveFormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
     ActiveVendorComponent
   ],
   imports: [
     CommonModule,
-    VendorRoutingModule
+    VendorRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class VendorModule { }
